efd9b3aa29c1c35c80f301a0367c67685c594054
trevor-r-allen <tallen2699@gmail.com>
2020-12-09 09:41:46 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// WEEK 2 DAY 2: COMPLEX OBJECT
+let mouse = {
+  color: "gold",
+  isOn: true,
+  yearsOwned: 2,
+  buttons: ["left", "right", "scroll"]
+}
+
+
+// WEEK 2 DAY 3: EVEN OR ODD
+
+function evenOrOdd(num){
+  console.log((num % 2 == 0) ? "Is Even" : "Is Odd")
+  return ((num % 2 == 0) ? "Is Even" : "Is Odd")
+}
+
+evenOrOdd(2)
+ evenOrOdd(3)
